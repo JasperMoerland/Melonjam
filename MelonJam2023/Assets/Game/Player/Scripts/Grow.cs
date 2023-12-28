adb9a144e1e6f7d0aa73a61ac6a6b72756cd2059
FanaticZdev <fanaticzdev7@gmail.com>
2023-12-28 18:00:55 +0100

    added first attack

---
+++ MelonJam2023/Assets/Game/Player/Scripts/Grow.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Sprites;
+
+public class Grow : MonoBehaviour
+{
+    public float targetSize, growSpeed, fadeSpeed;
+    public SpriteRenderer sprite;
+
+    private bool ableToHit = true;
+    private new Collider2D collider;
+
+    private void Start()
+    {
+        collider = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(targetSize, targetSize, 1), Time.deltaTime * growSpeed);
+        float alpha = Mathf.Lerp(sprite.color.a, 0, Time.deltaTime * fadeSpeed);
+        sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, alpha);
+        if (sprite.color.a < 0.5) ableToHit = false;
+        if (sprite.color.a < 0.05) Destroy(gameObject);
+    }
+    private void OnTriggerEnter2D(Collider2D collisionEnemy)
+    {
+        {
+            Debug.Log("hit");
+            Debug.Log(collisionEnemy.gameObject.name);
+            if (collisionEnemy.gameObject.name == "Player") return;
+            Destroy(gameObject);
+        }
+    }
+}
