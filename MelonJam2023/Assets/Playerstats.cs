fa6e16c1bce0564f89263666e1d9b3beb35d79f9
JasperMoerland <155015142+JasperMoerland@users.noreply.github.com>
2023-12-28 17:08:38 +0100

    h

---
+++ MelonJam2023/Assets/Playerstats.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Playerstats : MonoBehaviour
+{
+    public float Health;
+    public float MaxHealth;
+    public float Damage;
+    public string EquipedCharm;
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        //HEALING
+        if (timer >= 0.1f)
+        {
+            if (Health + (MaxHealth / 200) <= MaxHealth)
+            {
+                Health += (MaxHealth / 200);
+            }
+            timer = 0;
+            
+        }
+        //dying, run any code here you want!
+        if (Health == 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+        }
+    }
+    public void DamageTEst()
+    {
+        Health -= 10;
+    }
+}
